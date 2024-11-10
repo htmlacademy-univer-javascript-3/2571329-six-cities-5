3928aec4f46e9ecfb4d43b7c9c43ef531ff6bb47
Ilyasov Viktor <ilasovviktor2666@gmail.com>
2024-11-11 01:27:55 +0300

    feat: add Map, Reviews and Near Card List to Offer page

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import { NotFound } from './pages/not-found/NotFound';
 import { PrivateRoute } from './components/private-router/PrivateRouter';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { OFFERS_CARDS, CITY } from './mocks/offers';
+import { REVIEWS } from './mocks/reviews';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -41,7 +42,7 @@ root.render(
         />
         <Route
           path={AppRoute.Offer}
-          element={<Offer />}
+          element={<Offer reviews={REVIEWS} offers={OFFERS_CARDS} currentCity={CITY}/>}
         />
         <Route
           path='*'
