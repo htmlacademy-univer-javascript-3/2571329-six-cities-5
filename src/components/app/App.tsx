a348ba993dca836511daff08913bcc4d18b89164
Ilyasov Viktor <ilasovviktor2666@gmail.com>
2024-11-11 21:18:46 +0300

    feat: add redux to project and realize two action for change selected city and fill offers on Main page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,55 @@
+import { useAppSelector } from '../../hooks';
 import { Main } from '../../pages/main/Main';
-import { CityData, offerCard } from '../../types';
+import { AppRoute, AuthorizationStatus } from '../../types/index';
+import { Login } from '../../pages/login/Login';
+import { Favorites } from '../../pages/favorites/Favorites';
+import { Offer } from '../../pages/offer/Offer';
+import { NotFound } from '../../pages/not-found/NotFound';
+import { PrivateRoute } from '../../components/private-router/PrivateRouter';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { REVIEWS } from '../../mocks/reviews';
+// import { CityData, offerCard } from '../../types';
 
-interface IAppProps {
-  offers: offerCard[];
-  currentCity: CityData;
-}
+// interface IAppProps {
+//   offers: offerCard[];
+//   currentCity: CityData;
+// }
 
-export const App: React.FC<IAppProps> = ({ offers, currentCity }) => (
-  <Main offers={offers} currentCity={currentCity} />
-);
+export const App: React.FC = () => {
+  const currentCity = useAppSelector((state) => state.currentCity);
+  const offers = useAppSelector((state) => state.offers);
+  const cities = useAppSelector((state) => state.cities);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <Main offers={offers} currentCity={currentCity} cities={cities}/>
+          }
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+              <Favorites offers={offers}/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer reviews={REVIEWS} offers={offers} currentCity={currentCity}/>}
+        />
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AppRoute, AuthorizationStatus } from './types';
 import { App } from './components/app/App';
-import { Login } from './pages/login/Login';
-import { Favorites } from './pages/favorites/Favorites';
-import { Offer } from './pages/offer/Offer';
-import { NotFound } from './pages/not-found/NotFound';
-import { PrivateRoute } from './components/private-router/PrivateRouter';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { OFFERS_CARDS, CITY } from './mocks/offers';
-import { REVIEWS } from './mocks/reviews';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,38 +10,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoute.Main}
-          element={
-            <App
-              offers={OFFERS_CARDS}
-              currentCity={CITY}
-            />
-          }
-        />
-        <Route
-          path={AppRoute.Favorites}
-          element={
-            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
-              <Favorites offers={OFFERS_CARDS}/>
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={AppRoute.Login}
-          element={<Login />}
-        />
-        <Route
-          path={AppRoute.Offer}
-          element={<Offer reviews={REVIEWS} offers={OFFERS_CARDS} currentCity={CITY}/>}
-        />
-        <Route
-          path='*'
-          element={<NotFound />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
